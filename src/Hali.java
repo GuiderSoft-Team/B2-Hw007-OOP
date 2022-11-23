1d9505eb45891bd52932d500e1b9d3a03cc0e0d3
Çağla <mariliith@hotmail.com>
2022-11-23 19:56:36 +0300

    Çağlasu Büke Homework is done. 23/11/2022

---
+++ src/Hali.java
@@ -0,0 +1,28 @@
+public class Hali {
+    private double fiyat;
+
+    public Hali() {
+    }
+
+    public Hali(double fiyat) {
+        setFiyat(fiyat);
+    }
+
+    public double getFiyat() {
+        return fiyat;
+    }
+
+    public void setFiyat(double fiyat) {
+        if (fiyat < 0){
+            this.fiyat=0;
+        }else {
+            this.fiyat = fiyat;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Birim Fiyat= %5.2f",fiyat);
+    }
+}
