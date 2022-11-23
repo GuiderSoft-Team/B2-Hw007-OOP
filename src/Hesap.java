2926d19970ad057d8e49bfa78484de6a91b8b456
GİZEM YAYLAGÜL <gizemyaylagul@gmail.com>
2022-11-23 19:52:38 +0300

    Gizem Yaylagül Homework 7

---
+++ src/Hesap.java
@@ -0,0 +1,17 @@
+public class Hesap {
+    Zemin zemin=new Zemin();
+    Hali hali=new Hali();
+    public Hesap() {
+    }
+    public Hesap(Zemin zemin, Hali hali) {
+        this.zemin = zemin;
+        this.hali = hali;
+    }
+    public double getToplamMaliyet(){
+        return zemin.alanHesapla()*hali.getFiyat();
+    }
+    @Override
+    public String toString() {
+        return String.format ("toplam maliyet : "+getToplamMaliyet());
+    }
+}
