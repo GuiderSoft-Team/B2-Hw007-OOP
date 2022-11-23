a77ca2397a85c9f0292fd966b58e3c64b16ad8eb
Erkan Cihan YILMAZ <therkancihan@gmail.com>
2022-11-23 19:55:22 +0300

    Erkan Cihan YILMAZ has completed Nov. 23.2022

---
+++ src/Zemin.java
@@ -0,0 +1,62 @@
+public class Zemin {
+
+    private String aciklama;
+    private double genislik;
+    private double uzunluk;
+
+    public Zemin() {
+    }
+
+    public Zemin(String aciklama, double genislik, double uzunluk) {
+        this.aciklama = aciklama;
+        this.genislik = genislik;
+        this.uzunluk = uzunluk;
+    }
+
+    public String getAciklama() {
+        return aciklama;
+    }
+
+    public void setAciklama(String aciklama) {
+        this.aciklama = aciklama;
+    }
+
+    public double getGenislik() {
+        return genislik;
+    }
+
+    public void setGenislik(double genislik) {
+        if (genislik<0){
+            this.genislik=0;
+            }else {
+            this.genislik = genislik;
+        }
+
+
+    }
+
+    public double getUzunluk() {
+        return uzunluk;
+    }
+
+    public void setUzunluk(double uzunluk) {
+        if (uzunluk<0){
+            this.uzunluk=0;
+        }else {
+            this.uzunluk = uzunluk;
+        }
+
+        }
+        public double alanHesapla(){
+        return genislik*uzunluk;
+        }
+
+
+        @Override
+        public String toString(){
+            return String.format("%s[%5.2f*%5.2f]",aciklama,genislik,uzunluk);
+        }
+
+    }
+
+
