332af3e2e5680eb4dfbaf5af0f5a96cc625cd1e7
burcuturanugur <107644861+burcuturanugur@users.noreply.github.com>
2022-11-25 18:33:02 +0300

    Burcu Uğur - Homework7 is done.

---
+++ src/Hali.java
@@ -0,0 +1,24 @@
+public class Hali {
+    private double fiyat;
+
+    public Hali() {
+    }
+
+    public Hali(double fiyat) {
+        if (fiyat<0){
+            this.fiyat=0.0;
+        }else {
+        this.fiyat = fiyat;
+        }
+
+    }
+
+    public double getFiyat(){
+        return fiyat;
+    }
+
+    @Override
+    public String toString() {
+        return "Birim fiyatı: " +fiyat;
+    }
+}
