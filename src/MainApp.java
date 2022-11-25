a1687703b74f3468645b3fa91dd54114144fe3d0
Aysun Ardıç <aysnardc0706@gmail.com>
2022-11-25 17:19:16 +0300

    Aysun ardıc odev

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,13 @@
 public class MainApp {
     public static void main(String[] args) {
-        //Please read readme.md file
+        Zemin zemin= new Zemin("",4.0, 2.75);
+        Hali hali=new Hali(3.5);
+
+
+        Hesap hesap= new Hesap(zemin,hali);
+
+        System.out.println("Toplam maliyet: " + hesap.getToplamMaliyet());
+
 
     }
 }
---
+++ src/Zemin.java
@@ -0,0 +1,68 @@
+public class Zemin {
+
+    private String aciklama;
+    private double genislik;
+    private double uzunluk;
+
+    public Zemin() {
+
+    }
+
+    public Zemin(String aciklama, double genislik, double uzunluk) {
+    }
+
+    public Zemin(double genislik, double uzunluk) {
+    }
+
+    public String getAciklama() {
+        return aciklama;
+    }
+
+    public void setAciklama(String aciklama) {
+
+        this.aciklama = aciklama;
+
+    }
+
+    public double getGenislik() {
+        return genislik;
+    }
+
+    public void setGenislik(double genislik) {
+        if (genislik>0){
+            this.genislik = genislik;
+
+        }else {
+            this.genislik=0;
+        }
+
+    }
+
+    public double getUzunluk() {
+        return uzunluk;
+    }
+
+    public void setUzunluk(double uzunluk) {
+        if (uzunluk>0){
+            this.uzunluk=uzunluk;
+        }else {
+        this.uzunluk = 0;
+
+
+    }
+
+
+    }
+    public double alanHesapla(){
+        return uzunluk*genislik;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return   String.format("%s (%5.2f*%5.2f)",aciklama,uzunluk,genislik);
+    }
+}
+
+
