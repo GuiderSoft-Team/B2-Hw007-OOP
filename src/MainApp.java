533528689c5ae4bb9111ca8221a08b3b9d34efb9
Ismail Deniz AKAN <ismaildenizakan@gmail.com>
2022-11-30 14:00:18 +0300

    done

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,13 +1,10 @@
 public class MainApp {
     public static void main(String[] args) {
         //Please read readme.md file
-
         Hali hali = new Hali(3.5);
         Zemin zemin = new Zemin("Zemin1",2.75, 4.0);
         Hesap hesap = new Hesap(zemin, hali);
         System.out.print("Toplam maliyet: " + hesap.getToplamMaliyet()+"  ");
-
-
         hali = new Hali(1.5);
         zemin = new Zemin("Zemin2",5.4, 4.5);
         hesap = new Hesap(zemin, hali);
